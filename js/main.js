0c03ed32f651a6b86f02727a67715e823d916926
vinci-member <membervinci@gmail.com>
2021-04-23 11:57:55 +0900

    初期コミ

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const stalker = document.getElementById('stalker');
+document.addEventListener('mousemove', (e) => {
+	stalker.style.transform =
+		'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+});
+
+const a = document.querySelectorAll('a');
+
+a.forEach((e) => {
+	e.addEventListener('mouseenter', () => {
+		stalker.classList.add('active');
+	});
+	e.addEventListener('mouseleave', () => {
+		stalker.classList.remove('active');
+	});
+});
+
+const tl = new TimelineMax();
+
+tl.from('.main__title span', 3.6, {
+	y: 120,
+	ease: 'power4.out',
+	delay: 1,
+	stagger: {
+		amount: 0.3,
+	},
+});
+
+tl.from('.main__sub span', 1.8, {
+	y: 30,
+	ease: 'power4.out',
+	delay: -2,
+	stagger: {
+		amount: 0.3,
+	},
+});
+
+const scrollanime = document.querySelectorAll('.scrollanime');
+window.addEventListener('scroll', () => {
+	scrollanime.forEach((e) => {
+		const rect = e.getBoundingClientRect().top;
+		const scroll = window.pageYOffset || document.documentElement.scrollTop;
+		const offset = rect + scroll;
+		const windowHeight = window.innerHeight; // 現在のブラウザの高さ
+		if (scroll > offset - windowHeight + 150) {
+			e.classList.add('fadeInDown');
+		}
+	});
+});
+
+// humberger menu
+const hum = document.querySelector('.hamburger');
+hum.addEventListener('click', (e) => {
+	hum.classList.toggle('active');
+
+	const hasactive = hum.classList.contains('active');
+	const gmenusp = document.querySelector('.globalMenuSp');
+
+	if (hasactive) {
+		gmenusp.classList.add('active');
+	} else {
+		gmenusp.classList.remove('active');
+	}
+});
+
+//loading animation
+window.onload = function () {
+	const spinner = document.getElementById('loading');
+
+	// Add .loaded to .loading
+	spinner.classList.add('loaded');
+};
---
+++ js/workitem.js
@@ -0,0 +1,32 @@
+// スマホテキスト内容
+
+//upper js
+const upper = document.querySelector('.up_btn');
+const wright = document.querySelector('.workitem__right');
+upper.addEventListener('click', () => {
+	wright.classList.toggle('upper');
+	const hsup = wright.classList.contains('upper');
+	if (hsup) {
+		upper.innerHTML = '<i class="fas fa-angle-down"></i>';
+	} else {
+		upper.innerHTML = '<i class="fas fa-angle-up"></i>';
+	}
+});
+
+const ClassSlideshow = document.querySelector('.slideshow');
+const Slidewindow = document.querySelector('.slidewindow');
+const img = document.querySelectorAll('#slide');
+
+for (let i = 0; i < img.length; i++) {
+	let src = img[i].getAttribute('src');
+	img[i].addEventListener('click', () => {
+		Slidewindow.setAttribute('id', 'slidewindow');
+		ClassSlideshow.setAttribute('id', 'slideshow');
+		ClassSlideshow.setAttribute('src', src);
+	});
+	Slidewindow.addEventListener('click', () => {
+		Slidewindow.removeAttribute('id', 'slidewindow');
+		ClassSlideshow.removeAttribute('id', 'slideshow');
+		ClassSlideshow.removeAttribute('src', src);
+	});
+}
